a1a916b7bfa4492d1c01d5fcbfadeece3d95f13d
bobyharmoko <boby.harmoko@gmail.com>
2016-11-02 21:42:12 +0700

    training 3

---
+++ training-2/training_2_3.php
@@ -0,0 +1,11 @@
+<?php
+	$bulan = 1;
+	switch ($bulan) {
+		case 1:
+			echo 'Januari';
+			break;
+		default:
+			echo 'Tidak Valid';
+			break;
+	}
+?>
---
+++ training-3/training_3_1.php
@@ -0,0 +1,14 @@
+<?php
+	/*FOR
+	 * 
+	 * for(a;b;c){
+	 * 
+	 * }
+	 * a -> inisial nilai awal
+	 * b -> kondisi
+	 * c -> increment
+	 * */
+	for($i=0;$i<10;$i++){
+		echo $i;
+	}
+?>
