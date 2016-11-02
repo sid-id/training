999f34d77d5cf3caac72a0b3fe1b85d1d7ec0268
bobyharmoko <boby.harmoko@gmail.com>
2016-11-02 21:15:34 +0700

    training 2

---
+++ training-2/training_2_1.php
@@ -0,0 +1,24 @@
+<?php
+	if(true){
+		/*Jika kondisi bernilai true maka akan menjalankan kode dibagian ini*/
+		echo 'Kondisi Benar';
+	}else{
+		/*Jika Kondisi bernilai salah maka akan menjalan kode dibagian ini*/
+		echo 'Kondisi Salah';
+	}
+	
+	/*Operator AND*/
+	if(true && false){
+		/*operator and akan bernilai benar apabila setiap kondisi yang ada bernilai benar atau true*/
+	}
+	
+	/*Operator OR*/
+	if(true || false){
+		/*operator or akan bernilai true apabila salah satu kondisi sudah bernilai true*/
+	}
+	
+	/*Kombinasi*/
+	if((((TRUE&&FALSE) || TRUE) && FALSE && TRUE) || TRUE){
+		/*Kondisi tersebut akan menghasilkan true / false?*/
+	}
+?>
---
+++ training-2/training_2_2.php
@@ -0,0 +1,8 @@
+<?php
+	$val = '';
+	if($val==0){
+		echo 'Point 1';
+	}else{
+		echo 'Point 2';
+	}
+?>
